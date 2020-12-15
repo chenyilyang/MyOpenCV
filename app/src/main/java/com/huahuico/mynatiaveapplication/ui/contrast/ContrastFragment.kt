3f538c103d93c02563491dc0f6bc73aed5555c36
chenyilyang <yiliang@huahuico.com>
2020-12-15 14:33:38 +0800

    remove gpu

---
+++ app/src/main/java/com/huahuico/mynatiaveapplication/ui/contrast/ContrastFragment.kt
@@ -0,0 +1,39 @@
+package com.huahuico.mynatiaveapplication.ui.contrast
+
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.huahuico.mynatiaveapplication.BaseFragment
+import com.huahuico.mynatiaveapplication.Native
+import com.huahuico.mynatiaveapplication.R
+import kotlinx.android.synthetic.main.fragment_src_dst.*
+
+class ContrastFragment : BaseFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_src_dst, container, false)
+
+    override fun doSomething(bitmaps: Array<Bitmap>) : Array<Bitmap> {
+        return Array<Bitmap>(bitmaps.size) {
+            Native.blur(bitmaps[it])
+//            Native.laplacian(bitmaps[it])
+        }
+    }
+
+    override fun renderingDstImages(bitmaps: Array<Bitmap>) {
+        dstImage.setImageBitmap(bitmaps[0])
+//        saveBitmapToAlbum(requireContext(), bitmaps[0])
+    }
+
+    override fun renderingSrcImages(bitmaps: Array<Bitmap>) {
+        srcImage.setImageBitmap(bitmaps[0])
+    }
+
+    override fun getAssetsImageNames(): Array<String> {
+        return arrayOf("cat.jpg")
+    }
+}
